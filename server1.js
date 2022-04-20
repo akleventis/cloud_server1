0f1f1a36226145939d8db75a7b43751b39af2161
Alex Leventis <alex.leventis@bit.ly>
2022-04-19 23:29:03 -0400

    create server1

---
+++ server1.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const axios = require('axios')
+const Pool = require('pg').Pool
+const cors = require('cors');
+
+var cn = new Pool({
+    host: "localhost",
+    port: 5432,
+    database: "cloud_final",
+    user: 'aleventis'
+})
+
+const app = express()
+app.use(cors({origin: true}))
+const port = 8082
+
+const getGameData = async () => {
+    try {
+        const r = await cn.query(`SELECT * FROM rpc`);
+        if (r.rows[0].name === "server2"){
+            [r.rows[0], r.rows[1]] = [r.rows[1], r.rows[0]]
+        }
+        return r.rows
+    } catch (err) {
+        return err
+    }
+}
+
+const updateDB = async data => { 
+    var champ
+    var loser
+    if (data["champion"] === "tie") return
+    if (data["champion"]==="server 1") { champ = "server1" }
+    if (data["champion"]==="server 2") { champ = "server2" }
+    loser = champ === "server1" ? "server2" : "server1"
+
+    await cn.query(`UPDATE rpc SET wins=wins+1, current_streak=current_streak+1 WHERE name = $1`, [champ])
+    await cn.query(`UPDATE rpc SET current_streak=0 where name=$1`, [loser])
+
+    cn.query(`SELECT wins, current_streak, longest_win_streak FROM rpc where name = $1`, [champ], (err, res) => {
+       if (err) { return error }
+       [cs, ls] = [res.rows[0].current_streak, res.rows[0].longest_win_streak]
+       if (cs >= ls) {
+            cn.query(`UPDATE rpc SET longest_win_streak=$1 where name=$2`, [cs, champ])
+       }
+    })
+}
+
+const pickRandom = () => {
+    const rpc = ["rock", "paper", "scissors"];
+    return rpc[Math.floor(Math.random() * 3)];
+}
+
+// Picks random move (rock, paper scissors)
+// sends move to server2 and await it's response
+// receive and update database with result (win/lose)
+// return response data to client
+app.get('/send', async (req, res) => {
+    let move = pickRandom()
+
+    try {
+        // send move to server 2
+        let gameResult = await axios.post('http://localhost:8081', {move: move})
+        await updateDB(gameResult.data)
+        res.send(gameResult.data)
+    } catch (err) {
+        res.send(err)
+    }
+});
+
+// retrieve database data and return to client
+app.get('/results', async (req, res) => {
+    const results = await getGameData()
+    res.send(results)
+})
+
+app.listen(port, () => { console.log(`Listening on port ${port}`) })
